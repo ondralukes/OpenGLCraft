92144c14bb2ce35671a9e02d921b11b8ec19be4f
ondralukes <mail@ondralukes.cz>
2020-01-29 20:53:08 +0100

    Pressing E pauses the game and releases cursor

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 #include <thread>
 
 #if defined(WIN32)
-  #include <direct.h>
+#include <direct.h>
 #endif
 
 #include <GL/glew.h>
@@ -124,6 +124,11 @@ int main(){
   double maxDrawTime = 0;
   double maxGenTime = 0;
 
+  bool inGUI = false;
+
+  glm::mat4 projection;
+  glm::mat4 view;
+
   //Pointer for recalculation thread
   intvec3 * chunkPosPtr = new intvec3(0,0,0);
   std::thread * th = new std::thread(recalcThWork, chunkPosPtr);
@@ -133,217 +138,245 @@ int main(){
     float deltaTime =time-lastTime;
     lastTime = time;
 
+
     if(time - lastStatsTime > 2.0f){
       lastStatsTime = time;
       maxGenTime = 0;
       maxDrawTime = 0;
     }
-    double mouseX, mouseY;
-    glfwGetCursorPos(window, &mouseX, &mouseY);
-    glfwSetCursorPos(window, wWidth/2, wHeight/2);
 
-    if(xAngle == -10){
-      xAngle = saveManager->loadPlayerRot().x;
-      yAngle = saveManager->loadPlayerRot().y;
-    } else {
-      xAngle -= (mouseX - wWidth/2)/100;
-      yAngle -= (mouseY - wHeight/2)/100;
-    }
+    double genTime = 0, drawTime = 0;
+    int chunksDrawed = 0;
 
-    if(xAngle>M_PI*2)  xAngle -= M_PI*2;
-    if(yAngle>M_PI/2)  yAngle = M_PI/2;
-    if(xAngle<0)  xAngle += M_PI*2;
-    if(yAngle<-M_PI/2)  yAngle = -M_PI/2;
-
-    glm::vec3 camDirection(
-      cos(yAngle) * sin(xAngle),
-      sin(yAngle),
-      cos(yAngle) * cos(xAngle)
-    );
-
-    glm::vec3 right = glm::vec3(
-      sin(xAngle - M_PI/2.0f),
-      0,
-      cos(xAngle - M_PI/2.0f)
-    );
-
-    glm::vec3 forward = glm::vec3(
-      sin(xAngle),
-      0,
-      cos(xAngle)
-    );
-    glm::vec3 up = glm::cross( right, camDirection );
-
-    if(glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS||glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS){
-      camPos += forward*deltaTime*3.0f;
-    }
-    if(glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS||glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS){
-      camPos -= forward*deltaTime*3.0f;
-    }
-    if(glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS||glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS){
-      camPos -= right*deltaTime*3.0f;
-    }
-    if(glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS||glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS){
-      camPos += right*deltaTime*3.0f;
-    }
-
-    static bool spacePressed = false;
-    static bool inAir = false;
-    if(glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS&&!spacePressed){
-      if(!inAir) yVelocity += 5.0f;
-      spacePressed = true;
-      inAir = true;
-    } else if(glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_RELEASE){
-      spacePressed = false;
-    }
     intvec3 blockPos(
       round(camPos.x),
       ceil(camPos.y-camHeight-0.5f),
       round(camPos.z)
     );
 
-    static bool lMousePressed = false;
-    static double lMousePressTime;
-    static bool destroying;
-    static intvec3 lMousePressRemoveBlock;
-    int lMouseState = glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT);
-    intvec3 removePos;
-    if(lMouseState == GLFW_PRESS){
-      if(!lMousePressed){
-        lMousePressTime = glfwGetTime();
-        destroying = true;
+    intvec3 chunkPos(
+      floor(blockPos.x/CHUNK_SIZE),
+      floor(blockPos.y/CHUNK_SIZE) + 1,
+      floor(blockPos.z/CHUNK_SIZE)
+    );
+
+    // Clear the screen
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    static bool ePressed = false;
+    if(glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS && !ePressed){
+      inGUI = !inGUI;
+
+      if(inGUI){
+        //Release cursor
+        glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+      } else {
+        //Hide cursor
+        glfwSetCursorPos(window, wWidth/2, wHeight/2);
+        glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_HIDDEN);
+      }
+      ePressed = true;
+    } else if(glfwGetKey(window, GLFW_KEY_E) == GLFW_RELEASE){
+      ePressed = false;
+    }
+
+    if(!inGUI){
+      double mouseX, mouseY;
+      glfwGetCursorPos(window, &mouseX, &mouseY);
+      glfwSetCursorPos(window, wWidth/2, wHeight/2);
+
+      if(xAngle == -10){
+        xAngle = saveManager->loadPlayerRot().x;
+        yAngle = saveManager->loadPlayerRot().y;
+      } else {
+        xAngle -= (mouseX - wWidth/2)/100;
+        yAngle -= (mouseY - wHeight/2)/100;
       }
 
-      if(getLookingAt(camPos,camDirection,&removePos)){
-        if(!lMousePressed) lMousePressRemoveBlock = removePos;
-        if(removePos == lMousePressRemoveBlock){
-          removeBlock(removePos, time-lMousePressTime);
+      if(xAngle>M_PI*2)  xAngle -= M_PI*2;
+      if(yAngle>M_PI/2)  yAngle = M_PI/2;
+      if(xAngle<0)  xAngle += M_PI*2;
+      if(yAngle<-M_PI/2)  yAngle = -M_PI/2;
+
+      glm::vec3 camDirection(
+        cos(yAngle) * sin(xAngle),
+        sin(yAngle),
+        cos(yAngle) * cos(xAngle)
+      );
+
+      glm::vec3 right = glm::vec3(
+        sin(xAngle - M_PI/2.0f),
+        0,
+        cos(xAngle - M_PI/2.0f)
+      );
+
+      glm::vec3 forward = glm::vec3(
+        sin(xAngle),
+        0,
+        cos(xAngle)
+      );
+      glm::vec3 up = glm::cross( right, camDirection );
+
+      if(glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS||glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS){
+        camPos += forward*deltaTime*3.0f;
+      }
+      if(glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS||glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS){
+        camPos -= forward*deltaTime*3.0f;
+      }
+      if(glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS||glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS){
+        camPos -= right*deltaTime*3.0f;
+      }
+      if(glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS||glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS){
+        camPos += right*deltaTime*3.0f;
+      }
+
+      static bool spacePressed = false;
+      static bool inAir = false;
+      if(glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS&&!spacePressed){
+        if(!inAir) yVelocity += 5.0f;
+        spacePressed = true;
+        inAir = true;
+      } else if(glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_RELEASE){
+        spacePressed = false;
+      }
+
+
+      static bool lMousePressed = false;
+      static double lMousePressTime;
+      static bool destroying;
+      static intvec3 lMousePressRemoveBlock;
+      int lMouseState = glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT);
+      intvec3 removePos;
+      if(lMouseState == GLFW_PRESS){
+        if(!lMousePressed){
+          lMousePressTime = glfwGetTime();
+          destroying = true;
         }
-      }
-      lMousePressed = true;
-    }
-    if(lMouseState == GLFW_RELEASE || removePos != lMousePressRemoveBlock){
-      removeBlock(lMousePressRemoveBlock, 0.0f);
-      lMousePressed = false;
-    }
 
-    static bool rMousePressed = false;
-    int rMouseState = glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_RIGHT);
-    if(rMouseState == GLFW_PRESS&&!rMousePressed){
-      intvec3 placePos;
-      if(getLookingAt(camPos,camDirection,NULL,&placePos)){
-        if(placePos.x != blockPos.x || placePos.z != blockPos.z ||(placePos.y != blockPos.y+1 && placePos.y != blockPos.y+2)){
-          Blocks::block_type blType = Inventory::getSelectedBlock();
-          if(blType != Blocks::NONE){
-            Blocks::block_data blData;
-            blData.type = blType;
-            Blocks::Block * block = Blocks::Block::decodeBlock(blData, placePos, mvpID);
-            addBlock(placePos, block);
-            Inventory::remove(GUI::selectedItemIndex);
+        if(getLookingAt(camPos,camDirection,&removePos)){
+          if(!lMousePressed) lMousePressRemoveBlock = removePos;
+          if(removePos == lMousePressRemoveBlock){
+            removeBlock(removePos, time-lMousePressTime);
           }
         }
+        lMousePressed = true;
       }
-      rMousePressed = true;
-    } else if(rMouseState == GLFW_RELEASE){
-      rMousePressed = false;
-    }
+      if(lMouseState == GLFW_RELEASE || removePos != lMousePressRemoveBlock){
+        removeBlock(lMousePressRemoveBlock, 0.0f);
+        lMousePressed = false;
+      }
+
+      static bool rMousePressed = false;
+      int rMouseState = glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_RIGHT);
+      if(rMouseState == GLFW_PRESS&&!rMousePressed){
+        intvec3 placePos;
+        if(getLookingAt(camPos,camDirection,NULL,&placePos)){
+          if(placePos.x != blockPos.x || placePos.z != blockPos.z ||(placePos.y != blockPos.y+1 && placePos.y != blockPos.y+2)){
+            Blocks::block_type blType = Inventory::getSelectedBlock();
+            if(blType != Blocks::NONE){
+              Blocks::block_data blData;
+              blData.type = blType;
+              Blocks::Block * block = Blocks::Block::decodeBlock(blData, placePos, mvpID);
+              addBlock(placePos, block);
+              Inventory::remove(GUI::selectedItemIndex);
+            }
+          }
+        }
+        rMousePressed = true;
+      } else if(rMouseState == GLFW_RELEASE){
+        rMousePressed = false;
+      }
 
 
-    yVelocity -= deltaTime*9.81f;
-    inAir = true;
-    if(isBlock(blockPos)){
-      if(yVelocity < 0){
+      yVelocity -= deltaTime*9.81f;
+      inAir = true;
+      if(isBlock(blockPos)){
+        if(yVelocity < 0){
+          yVelocity = 0;
+          camPos.y = blockPos.y + 0.5f + camHeight;
+        }
+        inAir = false;
+      }
+
+      camPos.y += yVelocity * deltaTime;
+
+      intvec3 aboveBlockPos(
+        round(camPos.x),
+        ceil(camPos.y-camHeight+playerHeight-0.5),
+        round(camPos.z)
+      );
+      if(isBlock(aboveBlockPos)){
+        camPos.y = oldCamPos.y;
         yVelocity = 0;
-        camPos.y = blockPos.y + 0.5f + camHeight;
       }
-      inAir = false;
-    }
+      for(int h = 1;h<4;h++){
+        for(int d = -1;d<=1;d+=2){
+          for(int r = 0;r<2;r++){
+            float py;
+            if(h==1) py = floor(camPos.y-camHeight+0.55f);
+            if(h==2) py = floor(camPos.y-camHeight+1.35f);
+            if(h==3) py = ceil(camPos.y-camHeight+1.35f);
+            intvec3 p(
+              round(camPos.x+ (r==0?d:0)),
+              py,
+              round(camPos.z+ (r==1?d:0))
+            );
+            float dx =  camPos.x - (blockPos.x);
+            float dz =  camPos.z - (blockPos.z);
 
-    camPos.y += yVelocity * deltaTime;
-
-    intvec3 aboveBlockPos(
-      round(camPos.x),
-      ceil(camPos.y-camHeight+playerHeight-0.5),
-      round(camPos.z)
-    );
-    if(isBlock(aboveBlockPos)){
-      camPos.y = oldCamPos.y;
-      yVelocity = 0;
-    }
-    for(int h = 1;h<4;h++){
-      for(int d = -1;d<=1;d+=2){
-        for(int r = 0;r<2;r++){
-          float py;
-          if(h==1) py = floor(camPos.y-camHeight+0.55f);
-          if(h==2) py = floor(camPos.y-camHeight+1.35f);
-          if(h==3) py = ceil(camPos.y-camHeight+1.35f);
-          intvec3 p(
-            round(camPos.x+ (r==0?d:0)),
-            py,
-            round(camPos.z+ (r==1?d:0))
-          );
-          float dx =  camPos.x - (blockPos.x);
-          float dz =  camPos.z - (blockPos.z);
-
-          if(
-            (r==0&&d==1&&dx>0.35f)||
-            (r==0&&d==-1&&dx<-0.35f)||
-            (r==1&&d==1&&dz>0.35f)||
-            (r==1&&d==-1&&dz<-0.35f))
-            {
-              if(isBlock(p)){
-                if(r==0){
-                  if(d == 1 && oldCamPos.x < camPos.x){
-                    camPos.x = oldCamPos.x;
-                  }
-                  if(d == -1 && oldCamPos.x > camPos.x){
-                    camPos.x = oldCamPos.x;
-                  }
-                } else {
-                  if(d == 1 && oldCamPos.z < camPos.z){
-                    camPos.z = oldCamPos.z;
-                  }
-                  if(d == -1 && oldCamPos.z > camPos.z){
-                    camPos.z = oldCamPos.z;
+            if(
+              (r==0&&d==1&&dx>0.35f)||
+              (r==0&&d==-1&&dx<-0.35f)||
+              (r==1&&d==1&&dz>0.35f)||
+              (r==1&&d==-1&&dz<-0.35f))
+              {
+                if(isBlock(p)){
+                  if(r==0){
+                    if(d == 1 && oldCamPos.x < camPos.x){
+                      camPos.x = oldCamPos.x;
+                    }
+                    if(d == -1 && oldCamPos.x > camPos.x){
+                      camPos.x = oldCamPos.x;
+                    }
+                  } else {
+                    if(d == 1 && oldCamPos.z < camPos.z){
+                      camPos.z = oldCamPos.z;
+                    }
+                    if(d == -1 && oldCamPos.z > camPos.z){
+                      camPos.z = oldCamPos.z;
+                    }
                   }
                 }
               }
             }
+
           }
-
         }
-      }
-      oldCamPos = camPos;
-      glm::mat4 projection = glm::perspective(glm::radians(90.0f),wWidth/(float)wHeight, 0.01f, 100.0f);
+        oldCamPos = camPos;
+        projection = glm::perspective(glm::radians(90.0f),wWidth/(float)wHeight, 0.01f, 100.0f);
 
-      glm::mat4 view = glm::lookAt(
-        camPos,
-        camPos + camDirection,
-        up
-      );
-      double genTime, drawTime;
-      double start, end;
-      start = glfwGetTime();
-      WorldGenerator::generate(camPos,deltaTime);
-      DroppedBlock::updateAll(deltaTime, camPos);
-      end = glfwGetTime();
-      genTime = end - start;
-      if(genTime > maxGenTime) maxGenTime = genTime;
-      // Clear the screen
-      glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        view = glm::lookAt(
+          camPos,
+          camPos + camDirection,
+          up
+        );
+        double genTime, drawTime;
+        double start, end;
+        start = glfwGetTime();
+        WorldGenerator::generate(camPos,deltaTime);
+        DroppedBlock::updateAll(deltaTime, camPos);
+        end = glfwGetTime();
+        genTime = end - start;
+        if(genTime > maxGenTime) maxGenTime = genTime;
+
+        *chunkPosPtr = chunkPos;
+      }
 
       glUseProgram(shaderProgramID);
 
-      intvec3 chunkPos(
-        floor(blockPos.x/CHUNK_SIZE),
-        floor(blockPos.y/CHUNK_SIZE) + 1,
-        floor(blockPos.z/CHUNK_SIZE)
-      );
-
-      *chunkPosPtr = chunkPos;
-
+      double start, end;
       start = glfwGetTime();
-      int chunksDrawed = drawChunks(projection,view,chunkPos);
+      chunksDrawed = drawChunks(projection,view,chunkPos);
       DroppedBlock::drawAll(projection,view);
       GUI::draw();
       end = glfwGetTime();
@@ -409,52 +442,52 @@ int main(){
     return 0;
   }
 
-int drawChunks(glm::mat4 projection, glm::mat4 view, intvec3 chunkPos){
-  int chunksDrawed = 0;
-  for(int x =-8;x<8;x++){
-    for(int y =-4;y<4;y++){
-      for(int z =-8;z<8;z++){
-        intvec3 chP(
-          chunkPos.x + x,
-          chunkPos.y + y,
-          chunkPos.z + z
-        );
-        Chunk * ch = Chunk::getChunk(chP);
-        if(ch != NULL){
-          if(ch->doDraw != 0){
-            ch->draw(projection,view);
-            chunksDrawed++;
-          }
-        }
-      }
-    }
-  }
-  return chunksDrawed;
-}
-
-void scrollCallback(GLFWwindow* window, double x, double y){
-  GUI::selectedItemIndex += y;
-  GUI::selectedItemIndex = (GUI::selectedItemIndex + 8)%8;
-}
-
-void recalcThWork(intvec3 * chunkPos){
-  while(true){
+  int drawChunks(glm::mat4 projection, glm::mat4 view, intvec3 chunkPos){
+    int chunksDrawed = 0;
     for(int x =-8;x<8;x++){
       for(int y =-4;y<4;y++){
         for(int z =-8;z<8;z++){
           intvec3 chP(
-            chunkPos->x + x,
-            chunkPos->y + y,
-            chunkPos->z + z
+            chunkPos.x + x,
+            chunkPos.y + y,
+            chunkPos.z + z
           );
           Chunk * ch = Chunk::getChunk(chP);
           if(ch != NULL){
-            if(ch->shouldRecalculate){
-              ch->recalculate();
+            if(ch->doDraw != 0){
+              ch->draw(projection,view);
+              chunksDrawed++;
+            }
+          }
+        }
+      }
+    }
+    return chunksDrawed;
+  }
+
+  void scrollCallback(GLFWwindow* window, double x, double y){
+    GUI::selectedItemIndex += y;
+    GUI::selectedItemIndex = (GUI::selectedItemIndex + 8)%8;
+  }
+
+  void recalcThWork(intvec3 * chunkPos){
+    while(true){
+      for(int x =-8;x<8;x++){
+        for(int y =-4;y<4;y++){
+          for(int z =-8;z<8;z++){
+            intvec3 chP(
+              chunkPos->x + x,
+              chunkPos->y + y,
+              chunkPos->z + z
+            );
+            Chunk * ch = Chunk::getChunk(chP);
+            if(ch != NULL){
+              if(ch->shouldRecalculate){
+                ch->recalculate();
+              }
             }
           }
         }
       }
     }
   }
-}
