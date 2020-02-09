7977150b9102baef66ce101404045ad6974ab182
ondralukes <mail@ondralukes.cz>
2020-02-09 22:38:13 +0100

    New terrain generation (grass blocks only)

--- src/worldGenerator.hpp
+++ src/worldGenerator.hpp
@@ -2,18 +2,34 @@
 #include <stdlib.h>
 #include <vector>
 #include <math.h>
+#include <random>
 
 #include <glm/glm.hpp>
 #include <GL/glew.h>
 
 #include "structs.hpp"
 
+namespace WorldGeneratorUtils{
+  class Peak {
+  public:
+    Peak(int _x, int _z, float _a, float _c)
+      :x(_x), z(_z), a(_a), c(_c) {};
+    int x;
+    int z;
+    float a;
+    float c;
+    float getValueAt(int x, int z);
+  };
+}
+
+using namespace WorldGeneratorUtils;
+
 class WorldGenerator{
   public:
     static void generate(glm::vec3 pos, float deltaTime);
   private:
     static void generateTerrain(int ** map, int tx, int tz, int xSize, int ySize);
-    static bool getBorderHeight(int x, int z, intvec3 dir, int length, int * res);
-    static bool getHeightAt(int x, int z, int * res);
-    static void placeTrees(int x, int z, int xSize, int zSize, int count);
+    static void getPeaks(int tchx, int tchz, std::vector<Peak *> * peak);
+    static float getRandFloat(std::mt19937 * rnd);
+    static const int terrainChunkSize = 4;
 };
